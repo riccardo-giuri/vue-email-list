9dbc542b0160be762dfc1dd80976cc34d7c033ee
riccardo-giuri <richicryst@gmail.com>
2023-07-30 16:03:45 +0200

    printing the email list in the DOM using axios

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 Vue.createApp({
     data () {
         return {
-
+            emailList: []
         }
+    },
+
+    mounted() {
+        for(let i = 0; i < 10 ; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((axiosResp) => {
+                this.emailList.push(axiosResp.data.response);
+            });
+        }     
     }
 }).mount("#app");
